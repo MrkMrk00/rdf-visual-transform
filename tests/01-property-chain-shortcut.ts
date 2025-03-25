5392c3aaf9d007ff1da535195da82288a655f925
MrkMrk00 <mhau@outlook.cz>
2025-03-25 08:14:48 +0100

    Add Java server for RML transformations

--- tests/01-property-chain-shortcut.ts
+++ tests/01-property-chain-shortcut.ts
@@ -1,11 +1,36 @@
-import * as rml from "@comake/rmlmapper-js";
 import * as graphology from "graphology";
-import { beforeEach, describe, it } from "vitest";
+import { assert, beforeEach, describe, it } from "vitest";
 import { graphIntoJsonLd, insertQuadIntoGraph } from "../src/graph";
 import { RdfStreamingReader } from "../src/RdfStreamingReader";
 
+import { Quad } from "@rdfjs/types";
 import graphData from "../example-data/people-graph.ttl?raw";
 import peopleMapping from "../rml-templates/01-property-chain-shortcut.ttl?raw";
+
+async function transform(inputTriples: object[], rmlMapping: string) {
+    let response!: Response;
+
+    try {
+        response = await fetch("http://localhost:3000/transform", {
+            method: "POST",
+            body: JSON.stringify({
+                graph: JSON.stringify(inputTriples),
+                mapping: rmlMapping,
+            }),
+        }).then((r) => (r.ok ? r : Promise.reject(r)));
+    } catch (e) {
+        return { error: await (e as Response).text() };
+    }
+
+    const reader = new RdfStreamingReader();
+    const newTriples: Quad[] = [];
+
+    await reader.readFromString(await response.text(), "application/n-triples", (quad) => {
+        newTriples.push(quad);
+    });
+
+    return newTriples;
+}
 
 describe("Apply property chain shortcut pattern", () => {
     const reader = new RdfStreamingReader();
@@ -20,9 +45,12 @@ describe("Apply property chain shortcut pattern", () => {
     });
 
     it("creates a shorcut", async () => {
-        const input = JSON.stringify(graphIntoJsonLd(graph));
-        const output = await rml.parseTurtle(peopleMapping, { input });
+        const input = graphIntoJsonLd(graph);
+        const newTriples = await transform(input, peopleMapping);
 
-        console.log(output);
+        assert(!("error" in newTriples), (newTriples as { error: string }).error);
+
+        console.log(newTriples);
     });
 });
+
