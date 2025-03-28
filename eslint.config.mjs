d41006f4f4610e555b2a3d6f19809a80e61ca7a9
MrkMrk00 <mhau@outlook.cz>
2025-03-28 07:48:22 +0100

    add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    reactHooks.configs["recommended-latest"],
+    {
+        ...reactPlugin.configs.flat.recommended,
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    reactPlugin.configs.flat["jsx-runtime"],
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                },
+            ],
+        },
+    },
+);
