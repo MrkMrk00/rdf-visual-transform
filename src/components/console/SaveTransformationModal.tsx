93135af309701547be19fd530428b3ff64d0b877
MrkMrk00 <mhau@outlook.cz>
2025-07-24 09:02:37 +0200

    feat(transformations): saving transformation into localStorage

--- src/components/console/SaveTransformationModal.tsx
+++ src/components/console/SaveTransformationModal.tsx
@@ -6,7 +6,8 @@ import {
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog';
-import { ReactNode } from 'react';
+import { DialogDescription } from '@radix-ui/react-dialog';
+import { ReactNode, useState } from 'react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 
@@ -19,31 +20,52 @@ export function SaveTransformationModal({
     children,
     ...props
 }: SaveTransformationModalProps) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    const close = setIsOpen.bind(null, false);
+
     return (
-        <Dialog>
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogTrigger asChild>{children}</DialogTrigger>
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle>Save transformation</DialogTitle>
+                    {error !== null && (
+                        <DialogDescription className="text-red-500">
+                            {error}
+                        </DialogDescription>
+                    )}
                 </DialogHeader>
                 <form
+                    className="flex flex-col gap-2"
                     onSubmit={(ev) => {
                         ev.preventDefault();
 
-                        if (props.onSubmit) {
-                            const name = new FormData(ev.currentTarget).get(
-                                'name',
-                            ) as string;
+                        const name = new FormData(ev.currentTarget).get(
+                            'name',
+                        ) as string;
 
+                        if (!name) {
+                            setError(
+                                'A transformation must have a valid name.',
+                            );
+
+                            return;
+                        }
+
+                        if (props.onSubmit) {
                             props.onSubmit(name);
                         }
+
+                        close();
                     }}
                 >
                     <Input name="name" placeholder="Transformation name" />
+                    <DialogFooter>
+                        <Button type="submit">Save transformation</Button>
+                    </DialogFooter>
                 </form>
-                <DialogFooter>
-                    <Button>Save transformation</Button>
-                </DialogFooter>
             </DialogContent>
         </Dialog>
     );
