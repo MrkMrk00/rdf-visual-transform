86592eb0414341be96353ed1dfffa6cdd9a47c2b
MrkMrk00 <mhau@outlook.cz>
2025-09-30 10:21:41 +0200

    add argparse to python script

--- scripts/transform-xml-rdf.py
+++ scripts/transform-xml-rdf.py
@@ -3,22 +3,31 @@
 import sys
 import os
 import random
+import argparse
 import xml.etree.ElementTree as ET
 
 FILE_MAX_SIZE = 2_000_000
 
 
-def main(argv: list[str]) -> int:
-    if len(argv) != 2:
-        raise AssertionError("provide input file")
+def parse_args():
+    parser = argparse.ArgumentParser(sys.argv[0])
+    parser.add_argument('--subjects', '-s', default=None, type=int,
+                        help='the number of subjects to keep from the input file')
+    parser.add_argument('--filesize', '-f', default=None,
+                        type=int, help='approximation of how much of the file to keep')
+    parser.add_argument('input', type=str)
 
-    input_file = argv[1]
+    return parser.parse_args()
+
+
+def main() -> int:
+    args = parse_args()
+
+    input_file = args.input
     file_size = os.stat(input_file).st_size
-
-    keep_margin = min(FILE_MAX_SIZE / file_size, 1)
+    keep_margin = min(args.filesize or FILE_MAX_SIZE / file_size, 1)
 
     context = ET.iterparse(input_file, events=("start", "end"))
-
     _, root = next(context)
 
     attrs = " ".join(
@@ -27,9 +36,18 @@ def main(argv: list[str]) -> int:
     rdf_ns = 'xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"'
     sys.stdout.write(f"<rdf:RDF {attrs} {rdf_ns}>\n")
 
+    subject_count = 0
+
     for event, elem in context:
+        if args.subjects is not None and subject_count >= args.subjects:
+            elem.clear()
+            break
+
         if event == "end" and elem.tag.endswith("Description"):
-            if random.random() < keep_margin:
+            if args.subjects is not None and subject_count < args.subjects:
+                sys.stdout.write(ET.tostring(elem).decode())
+                subject_count += 1
+            elif args.filesize is not None and random.random() < keep_margin:
                 sys.stdout.write(ET.tostring(elem).decode())
 
             elem.clear()
@@ -41,4 +59,4 @@ def main(argv: list[str]) -> int:
 
 
 if __name__ == "__main__":
-    raise SystemExit(main(sys.argv))
+    raise SystemExit(main())
