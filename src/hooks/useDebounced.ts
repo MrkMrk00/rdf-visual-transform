317d992af17b6ad1184a5f3dd4e61f817204a5d8
MrkMrk00 <mhau@outlook.cz>
2025-10-13 12:24:17 +0200

    feat: useThrottledFunc

--- src/hooks/useDebounced.ts
+++ src/hooks/useDebounced.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export function useDebounced<T>(value: T, delay: number = 300): T {
     const [debouncedValue, setDebouncedValue] = useState(value);
@@ -14,4 +14,35 @@ export function useDebounced<T>(value: T, delay: number = 300): T {
     }, [value, delay]);
 
     return debouncedValue;
+}
+
+export function useThrottledFunc<TFunc extends (...args: TArgs) => void, TArgs extends any[]>(
+    func: TFunc,
+    timeout: number = 200,
+): (...args: TArgs) => void {
+    const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+    useEffect(() => {
+        return () => {
+            const timeout = timeoutRef.current;
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+        };
+    }, [func, timeout]);
+
+    return useCallback(
+        (...args: TArgs) => {
+            if (timeoutRef.current !== null) {
+                return;
+            }
+
+            timeoutRef.current = setTimeout(() => {
+                timeoutRef.current = null;
+            }, timeout);
+
+            func(...args);
+        },
+        [func, timeout],
+    );
 }
